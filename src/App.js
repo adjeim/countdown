4e448b69d50b4cc2f2928a70d855ef770e9a52c4
Mia Adjei <adjei.m@gmail.com>
2017-06-26 00:32:27 -0400

    Create inputs for user to set the timer.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Clock from './components/Clock/Clock';
-import SelectTime from './components/SelectTime/SelectTime';
 import Button from './components/Button/Button';
 import './App.css';
 
@@ -8,14 +7,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Countdown</h1>
-        <SelectTime />
+      <div className = "App">
+        <h1 className = 'title'>Countdown</h1>
         <Clock />
         <Button />
         <Button />
         <Button />
-
       </div>
     );
   }
--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import './Clock.css';
 
 class Clock extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hours: 0,
+			minutes: 0,
+			seconds: 0
+		}
+	}
+
+	setTimer() {
+
+	}
 
 	render() {
+		console.log(this.state.hours, 'hours', this.state.minutes, 'minutes', this.state.seconds, 'seconds');
+
 		return (
-			<div className='Clock-face'>
-				This is where the clock will go.
+			<div>
+	      <form className = 'SelectTime'>
+	      	<input type = 'number' min ='0' max = '24' default = { this.state.hours } /> hours
+	      	<input type = 'number' min ='0' max = '60' default = { this.state.minutes } /> minutes
+	      	<input type = 'number' min ='0' max = '60' default = { this.state.seconds } /> seconds
+	      </form>
+	      
+				<div className = 'Clock-face'>
+					This is where the clock will go.
+				</div>
 			</div>
+
 		);
 	}
 }
 
 export default Clock;
+
